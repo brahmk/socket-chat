522ac7ed9388b0e9b23956a2d257e7fca27a4304
brahmk <brahm.kelly@gmail.com>
2022-06-29 00:19:45 -0400

    first draft

---
+++ src/components/ChatRoom.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import useChat from "./useChat";
+
+export default function ChatRoom(props) {
+  const { roomId } = props.params;
+  const { messages, sendMessage } = useChat(roomId);
+  const [newMessage, setNewMessage] = useState("");
+  //
+
+  const handleNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    sendMessage(newMessage);
+    setNewMessage("");
+  };
+
+  return (
+    <div className="chat-room-container">
+      <h1 className="room-name">Room: {roomId}</h1>
+      <div className="messages-container">
+        <ol className="messages-list">
+          {messages.map((message, i) => (
+            <li
+              key={i}
+              className={`message-item ${
+                message.ownedByCurrentUser ? "my-message" : "received-message"
+              }`}
+            >
+              {message.body}
+            </li>
+          ))}
+        </ol>
+      </div>
+      <textarea
+        value={newMessage}
+        onChange={handleNewMessageChange}
+        className="new-message-input-field"
+        placeholder="Send a message"
+      />
+      <button onClick={handleSendMessage} className="send-button">
+        Send
+      </button>
+    </div>
+  );
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  const [roomName, setRoomName] = useState("");
+
+  const handleRoomChange = (event) => {
+    setRoomName(event.target.value);
+  };
+
+  return (
+    <div className="home-container">
+      <input
+        type="text"
+        placeholder="chat room"
+        value={roomName}
+        onChange={handleRoomChange}
+        className="text-input-field"
+      />
+      <Link to={`/${roomName}`} className="enter-room-button">
+        Join Chat
+      </Link>
+    </div>
+  );
+}
